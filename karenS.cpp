5a4a5052c0b6e58b1269024ef2c0a0d6afa8a7ce
blueberrykary <ksalinas3@csub.edu>
2017-06-24 14:24:34 -0700

    Added new functions

--- karenS.cpp
+++ karenS.cpp
@@ -1,8 +1,7 @@
 //Modified by: Karen Salinas
-//Date: 6/21/2017
-
+//Modified Date: 6/24/2017
+//Week 4
 //On this program, I am in charge of the background.
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,11 +19,6 @@
 #include "game.h"
 
 using namespace std;
-
-Timers() {
-    physicsRate = 1.0 / 30.0;
-    oobillion = 1.0 /1e9;
-}
 
 //This prototype allows background movement
 void applyBackgroundMovement(void) {
@@ -46,27 +40,67 @@ void applyBackgroundMovement(void) {
 
 //rendering the Background
 void renderBackground(void) {
+    //Rect r;
+    //Clear the screen
+    glClearColor(0.2, 0.2, 0.1, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //float cx = gl.xres/2.0;
+    //float cy = gl.yres/2.0;
     
     glBegin(GL_QUADS);
         glColor3f(0.2, 0.2, 0.2);
         glVertex2i(0,   200);
         glVertex2i(gl.xres,200);
-        glVertex2i(0.4,0.4,0.4);
+       // glVertex2i(0.4,0.4,0.4);
         glVertex2i(gl.xres,0);
         glVertex2i(0,   0);
     glEnd();
     //show boxes as background
-    for (int i = 0; i < 20; i++) {
-        glPushMatrix();
-        glTranslated(gl.box[i][0], gl.box[i][1], gl.box[i][2]);
-        glColor3f(0.2, 0.2, 0.2);
-        glBegin(GL_QUADS);
-                glVertex2i( 0, 0);
-                glVertex2i( 0, 30);
-                glVertex2i(20, 30);
-                glVertex2i(20, 0);
-        glEnd();
-        glPopMatrix();
+   
     }
+//}
+//Will display background color variation
+void displayColors() {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_QUADS);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex2f(-0.8f, 0.1f);
+        glVertex2f(-0.2f, 0.1f);
+        glVertex2f(-0.2f, 0.7f);
+        glVertex2f(-0.8f, 0.7f);
+
+        //glVertex2f(0.0f, 1.0f, 0.0f);
+        glVertex2f(-0.7f, -0.6f);
+        glVertex2f(-0.1f, -0.6f);
+        glVertex2f(-0.1f, 0.0f);
+        glVertex2f(-0.8f, 0.0f);
+    
+        glColor3f(0.2f, 0.2f, 0.2f);
+        glVertex2f(-0.9f, -0.7f);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f(-0.5f, -0.7f);
+        glColor3f(0.2f, 0.2f, 0.2f);
+        glVertex2f(-0.5f, -0.3f);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f(-0.9f, -0.3f);
+        glEnd();
+
+        glFlush();
 }
 
+void FlashlightPower() {
+    //Displays the text for amount of power flashlight currently has
+    Rect r;
+    ggprint8b(&r, 16, 0x00ffff44, "Battery Power");
+}
+
+void drawFlashlightPower(float power) {
+    //This will display the Flashlight's battery power
+    glBegin(GL_QUADS);
+        glColor3f(1, 0, 0);
+        glVertex2f(0, 0);
+        glVertex2f(1, 0);
+        glVertex2f(1, power);
+        glVertex2f(0, power);
+    glEnd();
+}
